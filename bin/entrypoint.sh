17d48b001c4a801c62f0d5aeeee9b9d9ffaf70d3
Dennis Hermsmeier <dennis.hermsmeier@gmail.com>
2019-05-05 10:43:42 +0200

    fix wrong volume var

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -25,7 +25,7 @@ function copy_missing_file {
 if [[ ! "$(id -u "${STEAM_USER}")" -eq "${STEAM_UID}" ]] || [[ ! "$(id -g "${STEAM_GROUP}")" -eq "${STEAM_GID}" ]] ; then
 	sudo usermod -o -u "${STEAM_UID}" "${STEAM_USER}"
 	sudo groupmod -o -g "${STEAM_GID}" "${STEAM_GROUP}"
-	sudo chown -R "${STEAM_USER}":"${STEAM_GROUP}" "${ARK_SERVER_VOLUME}" "${STEAM_HOME}"
+	sudo chown -R "${STEAM_USER}":"${STEAM_GROUP}" "${SERVER_VOLUME}" "${STEAM_HOME}"
 fi
 
 echo "_______________________________________"
